ff3b5e154593500331546a8c859e1f93bd8183c6
Magesh08 <mageshvaran16@gmail.com>
2023-01-13 18:04:17 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>Search The Movie</h1>
-      <h2>You Like</h2>
+      <h1>You Like</h1>
 
       <div className="search">
         <input
